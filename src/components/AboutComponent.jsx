7de0ef9e8ff37aaeb0ef718aae0eee3f58e7c0c1
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-08-22 11:11:31 +0530

    changed the border color and design

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -8,9 +8,9 @@ const AboutComponent = () => {
     <div className='bg-purple-900 w-full min-h-screen flex flex-col justify-center'>
       <div className='flex flex-col xl:flex-row lg:px-10'>
           <div className='flex justify-center xl:justify-start xl:items-center w-full xl:w-1/2 px-4 sm:px-8 lg:px-10'> 
-            <img src={profilePhoto} alt='My Profile Pic' className='w-40 h-40 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 xl:w-96 xl:h-96 rounded-full mt-32 mb-8 sm:mt-36 md:mt-40 lg:mt-36 lg:ml-10 border-b-10 border-lime-400 pb-1'></img>
+            <img src={profilePhoto} alt='My Profile Pic' className='w-40 h-40 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 xl:w-96 xl:h-96 rounded-full mt-32 mb-8 sm:mt-36 md:mt-40 lg:mt-36 lg:ml-10 border-16 border-double border-yellow-400 pb-1'></img>
           </div>
-          <div className='flex flex-col justify-center w-full xl:w-3/4 px-6 lg:px-24 xl:pt-28 gap-4'>
+          <div className='flex flex-col justify-center w-full xl:w-3/4 px-6 lg:px-24 xl:pt-24 gap-4'>
               <h1 className='text-3xl sm:text-4xl font-el-messiri font-bold tracking-wider text-white flex-wrap text-center leading-snug animate-moveDown text-shadow-lg text-shadow-black border-b-4 border-yellow-400 pb-1'>About Me</h1>
               <div className='text-justify text-lg md:text-xl animate-fadeIn text-white space-y-6 lg:space-y-8 xl:space-y-10'>
                 <p>Hello! I'm <b className='text-yellow-400'>Sakshi Rastogi</b>, a frontend developer passionate about crafting sleek, responsive, and user-friendly web interfaces. With over a year of experience in <b className='text-lime-400'>HTML</b>, <b className='text-lime-400'>CSS</b>, <b className='text-lime-400'>JavaScript</b>, <b className='text-lime-400'>TypeScript</b> and <b className='text-lime-400'>React.js</b>, I specialize in transforming ideas into seamless digital experiences.</p>
