043c52416b10e28a8b46c120d539f14d48b77764
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-08-22 22:53:42 +0530

    divided technical skills in 3 parts (frontend, testing, tools)

---
+++ src/components/TechnicalSkillComponent.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import htmlLogo from "../assets/html-logo.png"
+import cssLogo from "../assets/css-logo.png"
+import tailwindcssLogo from "../assets/tailwindcss-logo.png"
+import jestLogo from "../assets/jest-logo.png"
+import typescriptLogo from "../assets/typescript-logo.png"
+import reactLogo from "../assets/react-logo.png"
+import jsLogo from "../assets/js-logo.png"
+import githubLogo from "../assets/github-logo.png"
+import vsCodeLogo from "../assets/vscode-logo.png"
+import SkillBarComponent from './SkillBarComponent'
+
+const TechnicalSkillComponent = () => {
+  return (
+     <div className="space-y-10">
+        {/* Frontend Section */}
+        <section>
+            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-2 border-yellow-400 pb-1">Frontend</h2>
+            <div className="space-y-4 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
+                <SkillBarComponent skill="HTML5" percentage={90} color="bg-lime-400" imageUrl={htmlLogo} />
+                <SkillBarComponent skill="CSS3" percentage={85} color="bg-lime-400" imageUrl={cssLogo}/>
+                <SkillBarComponent skill="JavaScript (ES6+)" percentage={80} color="bg-lime-400" imageUrl={jsLogo} />
+                <SkillBarComponent skill="TypeScript" percentage={75} color="bg-lime-400" imageUrl={typescriptLogo}/>
+                <SkillBarComponent skill="Tailwind CSS" percentage={90} color="bg-lime-400" imageUrl={tailwindcssLogo}/>
+                <SkillBarComponent skill="React.js" percentage={85} color="bg-lime-400" imageUrl={reactLogo}/>
+            </div>
+        </section>
+
+        {/* Testing */}
+        <section>
+            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-2 border-yellow-400 pb-1">Testing</h2>
+            <div className="space-y-4 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
+                <SkillBarComponent skill="Jest" percentage={70} color="bg-red-400" imageUrl={jestLogo}/>
+            </div>
+        </section>
+
+        {/* Tools Section */}
+        <section>
+            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-2 border-yellow-400 pb-1">Tools & Others</h2>
+            <div className="space-y-4 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
+            <SkillBarComponent skill="GitHub" percentage={85} color="bg-indigo-400" imageUrl={githubLogo}/>
+            <SkillBarComponent skill="VS Code" percentage={90} color="bg-indigo-400" imageUrl={vsCodeLogo}/>
+            </div>
+        </section>
+    </div>
+  );
+};
+
+export default TechnicalSkillComponent
