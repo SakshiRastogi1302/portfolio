d4cea61cdcec5990b64261f64498675e5f2c7338
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-08-22 22:52:13 +0530

    created a component that showcase the skill name, its image, knowledge in % and progress bar

---
+++ src/components/SkillBarComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SkillBarComponent = ({ skill, percentage, color, imageUrl }) => {
+  return (
+     <div>
+      <div className="flex justify-between mb-1">
+        <span><img src={imageUrl} className='w-10 hover:scale-110 transition-transform'></img></span>
+        <span>{skill}</span>
+        <span>{percentage}%</span>
+      </div>
+      <div className="w-full h-2 bg-gray-700 rounded-full">
+         <div className={`h-2 rounded-full ${color}`} style={{ width: `${percentage}%` }}></div>
+      </div>
+    </div>
+  )
+}
+
+export default SkillBarComponent
