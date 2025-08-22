570cdbbc47fb44e54f0b7bbfef0ef3711ca308c7
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-08-22 22:52:54 +0530

    created skill component showcasing technical skills as well as skill on which i am currently exploring.

--- src/components/SkillsComponent.jsx
+++ src/components/SkillsComponent.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import TechnicalSkillComponent from './TechnicalSkillComponent'
 
 const SkillsComponent = () => {
   return (
-    <div>SkillsComponent</div>
+    <div className="bg-purple-900 w-full min-h-screen text-white flex flex-col items-center py-16 px-6">
+        {/* Heading */}
+        <h1 className="text-2xl sm:text-4xl font-el-messiri font-bold tracking-wider animate-moveDown text-shadow-lg text-shadow-black border-b-4 border-yellow-400 pb-1 my-12">Skills</h1>
+        <div className="w-full max-w-6xl flex flex-col gap-10">
+          
+          {/* 1. Technical Skills with Progress Bars */}
+          <section>
+            <TechnicalSkillComponent />
+          </section>
+
+          {/* 2. Currently Learning */}
+          <section>
+            <h2 className="text-xl md:text-2xl font-semibold mb-6 text-center border-b-2 border-yellow-400 pb-1">Currently Exploring</h2>
+            <div className="flex justify-center md:justify-start gap-6 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
+              <span className="bg-yellow-500 text-black px-4 py-2 rounded-lg shadow-md"><i className="fa-brands fa-java text-2xl mr-2 text-red-600"></i>Java</span>
+              <span className="bg-green-700 text-black px-4 py-2 rounded-lg shadow-md"><i className="fa-brands fa-node-js text-2xl mr-2 text-lime-400"></i>Node.js</span>
+            </div>
+          </section>
+        </div>
+    </div>
+
   )
 }
 
