6102487b2565dc1d60e37d0eeb26d48e14a5ef93
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-08-24 20:49:50 +0530

    stored work experience details in an array of object.

---
+++ src/utils/workExperienceData.js
@@ -0,0 +1,19 @@
+const workExperienceData = [
+  {
+    id: 1,
+    role: "Full Stack Developer",
+    company: "Salescode.ai (formerly known as Applicate AI)",
+    duration: "Aug 2021 - Oct 2022",
+    description: [
+        "Developed the camera and QR code scanner feature for Android and iOS apps using cordova plugin.",
+        "Developed and integrated services that fetch data from the API once and store it in the database,resulting in a 40% improvement in the app's speed.",
+        "Replaced over 100+ axios requests with custom HTTP methods for each request type, minimizing overhead from third-party libraries like Axios.",
+        "Developed a query builder service in Spring Boot which processes JSON input from the UI to dynamically generate sql queries.",
+        "Migrated the business logic code written in JavaScript to TypeScript, enhancing readability and maintainability by separating UI rendering from service-oriented code.",
+        "Developed a unified React.js service that handles pre and post login configurations ensuring seamless app initialization and user experience.",
+"       Created SQL queries for generating master reports to present data on the UI.",
+    ]
+  },
+];
+
+export default workExperienceData;
