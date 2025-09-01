10dbd595f896550482af3b4b43f510a3bb226586
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-09-01 22:33:18 +0530

    removed unused font family and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,7 @@ module.exports = {
   theme: {
     fontFamily: {
       'mulish': ['Mulish', 'sans-serif'],
-      'dancing-script': ['Dancing Script', 'cursive'],
       'el-messiri': ["El Messiri", "sans-serif"],
-      "lora": ["Lora", "serif"],
     },
     textShadow: {
       sm: '0 1px 2px rgb(0 0 0 / 0.25)',
@@ -17,19 +15,14 @@ module.exports = {
     },
     extend: {
       animation: {
-        moveLine: 'moveLine 6s linear forwards',
         wave: 'wave 2s linear infinite',
         moveDown: 'moveDown 1s linear forwards',
         fadeInLeft: 'fadeInLeft 3s linear forwards',
         fadeInRight: 'fadeInRight 3s linear forwards',
         fadeInDown: 'fadeInDown 2s linear forwards',
         fadeIn: 'fadeIn 4s linear forwards',
-        rotate: 'rotate 5s infinite',
-        pendulum:'pendulum 8s infinite linear',
-        progressBar:'progressBar 2s linear forwards'
       },
       keyframes: {
-        moveLine: { '0%': { height: 0 }, '100%': { height: '55%' } },
         wave: {
           '0%': { transform: 'rotate(0.0deg)' },
           '10%': { transform: 'rotate(14deg)' },
@@ -63,31 +56,9 @@ module.exports = {
           '75%': { opacity: 0.5 },
           '100%': { opacity: 1 }
         },
-        rotate: {
-          '15%': { transform: 'rotate(45deg)' },
-          '30%': { transform: 'rotate(90deg)' },
-          '45%': { transform: 'rotate(180deg)' },
-          '75%': { transform: 'rotate(270deg)' },
-          '100%': { transform: 'rotate(360deg)' }
-        },
-        pendulum: {
-          '0% , 100%': { transform: 'rotateY(15deg)'},
-          '50%': { transform: 'rotateY(-15deg)'}
-        },
-        progressBar: {
-          '0%':{width:'10%'},
-          '50%':{width:'50%'},
-          '100%':{width:'90%'}
-        }
       }
     },
   },
-  plugins: [function ({ addUtilities, theme }) {
-      const shadows = theme('textShadow')
-      const utilities = Object.keys(shadows).map(key => ({
-        [`.text-shadow-${key}`]: { textShadow: shadows[key] },
-      }))
-      addUtilities(utilities)
-    },],
-    darkMode:"class"
+  plugins: [],
+  darkMode:"class"
 }
