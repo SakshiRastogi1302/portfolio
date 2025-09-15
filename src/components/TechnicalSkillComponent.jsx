7830b20e2a9bf6b26674d6461cc71955baf85127
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-09-15 17:37:41 +0530

    removed tools and testing section, made frontend section responsive across all devices using grid

--- src/components/TechnicalSkillComponent.jsx
+++ src/components/TechnicalSkillComponent.jsx
@@ -12,34 +12,20 @@ import SkillBarComponent from './SkillBarComponent'
 
 const TechnicalSkillComponent = () => {
   return (
-     <div className="space-y-10">
-        {/* Frontend Section */}
+    <div className="space-y-8">
+        {/* Frontend + Testing + Tools Section */}
         <section>
-            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-2 border-yellow-400 pb-1">Frontend</h2>
-            <div className="space-y-4 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
+            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-4 border-yellow-600 dark:border-yellow-400 pb-1">Frontend + Testing + Tools</h2>
+            <div className="grid grid-col-1 md:grid-cols-2 gap-x-6 gap-y-5 shadow-2xl shadow-black bg-black text-white border-2 border-white p-6 rounded-2xl">
                 <SkillBarComponent skill="HTML5" percentage={90} imageUrl={htmlLogo} />
                 <SkillBarComponent skill="CSS3" percentage={85} imageUrl={cssLogo}/>
                 <SkillBarComponent skill="JavaScript (ES6+)" percentage={80} imageUrl={jsLogo} />
                 <SkillBarComponent skill="TypeScript" percentage={75} imageUrl={typescriptLogo}/>
                 <SkillBarComponent skill="Tailwind CSS" percentage={90} imageUrl={tailwindcssLogo}/>
                 <SkillBarComponent skill="React.js" percentage={85} imageUrl={reactLogo}/>
-            </div>
-        </section>
-
-        {/* Testing */}
-        <section>
-            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-2 border-yellow-400 pb-1">Testing</h2>
-            <div className="space-y-4 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
                 <SkillBarComponent skill="Jest" percentage={70} imageUrl={jestLogo}/>
-            </div>
-        </section>
-
-        {/* Tools Section */}
-        <section>
-            <h2 className="text-xl md:text-2xl font-semibold text-center mb-4 border-b-2 border-yellow-400 pb-1">Tools & Others</h2>
-            <div className="space-y-4 shadow-2xl shadow-black bg-black border-2 border-white p-4 rounded-2xl">
-            <SkillBarComponent skill="GitHub" percentage={85} imageUrl={githubLogo}/>
-            <SkillBarComponent skill="VS Code" percentage={90} imageUrl={vsCodeLogo}/>
+                <SkillBarComponent skill="GitHub" percentage={85} imageUrl={githubLogo}/>
+                <SkillBarComponent skill="VS Code" percentage={90} imageUrl={vsCodeLogo}/>
             </div>
         </section>
     </div>
