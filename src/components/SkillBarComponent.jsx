ae50f7c625677b4b08a03a2b41522001c3e4c7c9
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-09-15 17:33:56 +0530

    updated the size of image, added the animation to progress bar and upon hovering the skills icon size will increase.

--- src/components/SkillBarComponent.jsx
+++ src/components/SkillBarComponent.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const SkillBarComponent = ({ skill, percentage, color, imageUrl }) => {
+const SkillBarComponent = ({ skill, percentage, imageUrl }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+       setProgress(percentage);
+    },200);
+
+    return() => clearTimeout(timer);
+  },[percentage])
   return (
      <div>
       <div className="flex justify-between mb-1">
-        <span><img src={imageUrl} className='w-10 hover:scale-110 transition-transform'></img></span>
+        <span><img src={imageUrl} className='w-5 hover:scale-150 transition-transform'></img></span>
         <span>{skill}</span>
-        <span>{percentage}%</span>
+        <span>{progress}%</span>
       </div>
       <div className="w-full h-2 bg-gray-700 rounded-full">
-         <div className={`h-2 rounded-full bg-purple-500`} style={{ width: `${percentage}%` }}></div>
+         <div className="h-2 rounded-full bg-purple-500 transition-all duration-1000 ease-out" style={{ width: `${progress}%` }}></div>
       </div>
     </div>
   )
