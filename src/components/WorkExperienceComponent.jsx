dd2b4b08c5420528e6ec51ab57048ff99ff49656
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-09-18 13:27:25 +0530

    updated the background color in light theme, removed the background color from box in both the themes, added yellow color to important words in dark theme and to headings and underlines

--- src/components/WorkExperienceComponent.jsx
+++ src/components/WorkExperienceComponent.jsx
@@ -9,19 +9,19 @@ const WorkExperienceComponent = () => {
   const currentMode = useSelector(store => store.toggleLightDarkMode.currentMode);
   return (
     <div className={currentMode==="light"?"":"dark"}>
-      <div className="bg-purple-300 dark:bg-purple-900 w-full min-h-screen flex flex-col items-center py-16 px-6">
-        <h2 className="text-3xl sm:text-4xl font-el-messiri font-bold text-purple-950 dark:text-white tracking-wider flex-wrap text-center leading-snug animate-moveDown text-shadow-lg text-shadow-black mt-14 mb-6 border-b-4 border-yellow-400">Work Experience</h2>
-        <div className="w-full max-w-5xl flex flex-col gap-8">
+      <div className="bg-purple-200 dark:bg-purple-900 w-full min-h-screen flex flex-col items-center py-16 px-6">
+        <h2 className="text-3xl sm:text-4xl font-el-messiri font-bold text-purple-950 dark:text-white tracking-wider flex-wrap text-center leading-snug animate-moveDown text-shadow-lg text-shadow-black mt-14 mb-6 border-b-4 border-yellow-500 dark:border-yellow-400">Work Experience</h2>
+        <div className="w-full max-w-5xl flex flex-col gap-8 animate-fadeInLeft">
           {workExperienceData.map((exp, index) => (
             <div
               key={index}
-              className="bg-white dark:bg-purple-500 border-2 border-white rounded-2xl shadow-lg shadow-black p-6 flex flex-col md:flex-row gap-4 transition-transform hover:scale-[1.02]">
+              className="border-2 border-purple-950 dark:border-white rounded-2xl shadow-lg shadow-black p-6 flex flex-col md:flex-row gap-4 transition-transform hover:scale-[1.02]">
               {/* Left side - Logo, Role & Company */}
               <div className="flex-1">
-                <h3 className="text-2xl font-semibold flex items-center gap-2 text-purple-950 text-shadow-lg text-shadow-black">
+                <h3 className="text-2xl font-semibold flex items-center gap-2 text-purple-950 dark:text-purple-300 text-shadow-lg text-shadow-black">
                   <Briefcase size={28} /> {exp.role}
                 </h3>
-                <Link to="https://salescode.ai/" target="_blank" rel="noopener noreferrer"><p className="text-lg text-yellow-400 font-medium underline" title="Open Salecode.ai official website">{exp.company}</p></Link>
+                <Link to="https://salescode.ai/" target="_blank" rel="noopener noreferrer"><p className="text-lg text-yellow-500 dark:text-yellow-400 font-medium" title="Open Salecode.ai official website">{exp.company}</p></Link>
                 <p className="text-sm text-purple-950 dark:text-white flex items-center gap-2 mt-1">
                   <Calendar size={20}/> {exp.duration}
                 </p>
@@ -38,7 +38,7 @@ const WorkExperienceComponent = () => {
                         __html: point.replace(
                           /\b(\d+(\+|%)?|improvement|improved|speed|performance|optimized|overhead|faster|readability|maintainability)\b/gi,
                           (match) =>
-                            `<span class="text-yellow-400 font-bold">${match}</span>`
+                            `<span class="text-yellow-500 dark:text-yellow-400 font-bold">${match}</span>`
                         ),
                       }}
                     />
